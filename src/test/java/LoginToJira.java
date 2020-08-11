927cfd71c3e4328e910741e35a156ef454a7944d
vs <shevtshov@gmail.com>
2020-08-11 18:38:19 +0300

    added recorder

--- src/test/java/LoginToJira.java
+++ src/test/java/LoginToJira.java
@@ -1,6 +1,10 @@
 
+import atu.testrecorder.ATUTestRecorder;
+import atu.testrecorder.exceptions.ATUTestRecorderException;
+import com.automation.remarks.video.annotations.Video;
 import com.codeborne.selenide.Configuration;
 
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import org.testng.annotations.Test;
@@ -14,20 +18,22 @@ public class LoginToJira {
     private LoginPage loginPage;
     private MainPage mainPage;
 
-
     @BeforeMethod()
-    public void setUp() {
+    public void setUp(){
         Configuration.browser = "Chrome";
         Configuration.reportsFolder = "screenshots";
     }
 
     @Test()
-    public void Login() {
+    public void Login() throws ATUTestRecorderException {
+        ATUTestRecorder recorder = new ATUTestRecorder("screenshots", "TestVideo", false);
+        recorder.start();
         loginPage = new LoginPage();
         mainPage = new MainPage();
         loginPage.openJira();
         loginPage.enterLoginAndPassword();
         loginPage.loginButton.click();
         mainPage.profileIsPresent();
+        recorder.stop();
     }
 }
